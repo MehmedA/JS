d0ae9f63ee9c565425a9bdc5d16204bc440df82a
MehmedA <mehmedak@gmail.com>
2023-01-13 23:10:32 +0300

    added email verification work

---
+++ Teamwork/TW08-Email_Validation/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const email = prompt("Please enter a valid email adress:");
+
+function isValidEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+console.log(isValidEmail(email));
