404eaaa8de2d389d8fb03ce1b821d8877177b805
MehmedA <mehmedak@gmail.com>
2023-01-27 22:59:31 +0300

    added registration table

--- Projects/homework/JS-03-PROJECT-03_CheckoutPage/App.js
+++ Projects/homework/JS-03-PROJECT-03_CheckoutPage/App.js
@@ -1,1 +1,33 @@
-const 
+"use script";
+
+// Buttons
+let plus = document.querySelectorAll(".plus");
+let minus = document.querySelectorAll(".minus");
+let remove = document.querySelectorAll(".remove");
+
+// Amount
+let quantity = document.querySelectorAll("#quantity");
+let stotal = document.querySelectorAll(".stotal");
+
+// Totals
+let subt = document.querySelector(".subt");
+let tax = document.querySelector(".tax");
+let ship = document.querySelector(".ship");
+let total = document.querySelector(".total");
+
+plus.forEach(p =>{
+    p.addEventListener("click", (e) =>{
+        e.preventDefault();
+        ++p.previousElementSibling.innerText;
+    })
+})
+
+minus.forEach(p =>{
+    p.addEventListener("click", (e) =>{
+        if (p.nextElementSibling.textContent > 1) {
+        e.preventDefault();
+        --p.nextElementSibling.innerText;
+    }})
+})
+
+(".stotal").innerText = "1"
